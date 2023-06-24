91bd7dcfa99ec5dc9ecea84ab574f2ce268e4f2d
hadysoufan <hadisoufan74@gmail.com>
2023-06-24 11:16:21 +0300

    Editing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>dsd</div>
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to the Store</h1>
+      </main>
+      <Footer />
+    </div>
   );
 }
 

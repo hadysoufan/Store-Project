b8166f8f0ea595df6a20376b163046b346c0d929
hadysoufan <hadisoufan74@gmail.com>
2023-06-24 12:33:03 +0300

    Adding Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './Screens/Home.screen';
+import ProductScreen from './Screens/Product.screen';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            {/* <Route path="/cart/:id?" element={<CartScreen />} /> */}
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 

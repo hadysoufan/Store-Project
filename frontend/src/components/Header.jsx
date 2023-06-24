3698d2f5460114299595d783c57e411e2672e20f
hadysoufan <hadisoufan74@gmail.com>
2023-06-24 11:34:06 +0300

    Customizing the Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function Header() {
   return (
-    <div>
-      <header>Header</header>
-    </div>
-  )
+    <header>
+      <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
+        <Container fluid>
+          <Navbar.Brand href="/">Store Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mr-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll>
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
-export default Header
+export default Header;

b22c404ca5298b0fe792f38546462989e21657af
hadysoufan <hadisoufan74@gmail.com>
2023-06-24 11:34:58 +0300

    Wrapping using Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -5,8 +6,10 @@ function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h1>Welcome to the Store</h1>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to the Store</h1>
+        </Container>
       </main>
       <Footer />
     </div>

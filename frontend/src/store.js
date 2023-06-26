782ca1c09b8a17b33ca5209e065eb45220608ea4
hadysoufan <hadisoufan74@gmail.com>
2023-06-26 20:57:29 +0300

    Importing userUpdateProfileReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,14 +6,21 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducer.js';
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducer.js';
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducer.js';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
-  userRegister: userRegisterReducer
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 
 // Load user info from local storage

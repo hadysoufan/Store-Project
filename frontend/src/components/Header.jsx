8204fe1542730ed4be3b3f50fb02268301cf4c70
hadysoufan <hadisoufan74@gmail.com>
2023-06-26 17:50:48 +0300

    Header component representing the navigation bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,25 +2,36 @@ import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
+/**
+ * Header component representing the navigation bar.
+ */
 function Header() {
   return (
     <header>
       <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
         <Container fluid>
+          {/* Logo */}
           <LinkContainer to="/">
             <Navbar.Brand>Store Shop</Navbar.Brand>
           </LinkContainer>
+
+          {/* Hamburger Menu */}
           <Navbar.Toggle aria-controls="navbarScroll" />
+
+          {/* Navigation Links */}
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="mr-auto my-2 my-lg-0"
               style={{ maxHeight: '100px' }}
               navbarScroll>
+              {/* Cart Link */}
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i>Cart
                 </Nav.Link>
               </LinkContainer>
+
+              {/* Login Link */}
               <LinkContainer to="/login">
                 <Nav.Link href="/login">
                   <i className="fas fa-user"></i>Login

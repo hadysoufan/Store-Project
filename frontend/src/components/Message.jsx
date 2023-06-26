cd77303a2dff540165d9a8196f596a90cbeddd63
hadysoufan <hadisoufan74@gmail.com>
2023-06-26 17:51:49 +0300

    Message component for displaying alert messages

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
 
+/**
+ * Message component for displaying alert messages.
+ * @param {Object} props - Component props.
+ * @param {string} props.variant - The variant of the alert (e.g., "success", "danger", "warning").
+ * @param {ReactNode} props.children - Child components representing the message content.
+ */
 function Message({ variant, children }) {
   return <Alert variant={variant}>{children}</Alert>;
 }

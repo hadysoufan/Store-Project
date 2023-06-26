29020e5083059efac4af2805056fe390b0615ef4
hadysoufan <hadisoufan74@gmail.com>
2023-06-26 18:01:24 +0300

    HomeScreen component displays the latest products on the home page

--- frontend/src/Screens/Home.screen.jsx
+++ frontend/src/Screens/Home.screen.jsx
@@ -6,12 +6,16 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { listProducts } from '../actions/productActions';
 
+/**
+ * The HomeScreen component displays the latest products on the home page.
+ */
 function HomeScreen() {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { error, loading, products } = productList;
 
   useEffect(() => {
+    // Fetches the list of products when the component mounts
     dispatch(listProducts());
   }, [dispatch]);
 
@@ -19,11 +23,14 @@ function HomeScreen() {
     <div>
       <h1>Latest Products</h1>
       {loading ? (
+        // Display a loading spinner while the products are being fetched
         <Loader />
       ) : error ? (
+        // Display an error message if there is an error fetching the products
         <Message variant="danger">{error}</Message>
       ) : (
         <Row>
+          {/* Render each product as a Product component */}
           {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />

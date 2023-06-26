6e5e26d5bf8c98c4c09120141458a66f4a0d28ee
hadysoufan <hadisoufan74@gmail.com>
2023-06-26 17:45:46 +0300

    Adding userLogin

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,11 +6,13 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducer.js';
+import { userLoginReducer } from './reducers/userReducer.js';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
